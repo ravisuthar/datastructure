36ba831dee14802ae55691957527da1da3e2cba8
sutha <!#bin/bash/git/88>
2022-08-14 18:30:27 +0530

    10 August 2022 - SinglyLinkedList programs

--- data-structure-algorithm/src/test/java/leetcode.must/doo/SinglyLinkedListTest.java
+++ data-structure-algorithm/src/test/java/leetcode.must/doo/SinglyLinkedListTest.java
@@ -5,18 +5,37 @@ import collections.SinglyLinkedList;
 public class SinglyLinkedListTest {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
-        list.getPopulatedList();
+       // list.getPopulatedList();
         //list.print();
        // System.out.println("Length is: " + list.getLength());
-        list.insertFirst(12);
+        //list.insertFirst(12);
         //list.print();
-        list.insertLast(20);
+       // list.insertLast(20);
+       // list.print();
+       // list.insert(200000,8);
+       // list.print();
+        //list.deleteFirst();
         //list.print();
-        list.insert(200000,8);
+        //list.deleteLast();
+        //list.print();
+
+       // list.delete(4);
+       // list.print();
+
+       // System.out.println(list.search(10));
+        //System.out.println(list.search(11));
+
+       // list.print(list.reverse());
+        //System.out.println(list.getMiddle());
+        //System.out.println(list.findNthFromEnd(10));
+
+
+        list.getPopulatedSortedList();
         list.print();
-        list.deleteFirst();
+        list.removeDuplicateFromSortedList();
         list.print();
-        list.deleteLast();
+
+        list.insertInSortedList(0);
         list.print();
     }
 }
