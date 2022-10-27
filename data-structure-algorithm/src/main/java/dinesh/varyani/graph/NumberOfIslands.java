2c8d0aec00c1fd31e3b223cef77a771302abd929
sutha <!#bin/bash/git/88>
2022-10-27 20:47:16 +0530

    27 Oct 2022 Trie

---
+++ data-structure-algorithm/src/main/java/dinesh/varyani/graph/NumberOfIslands.java
@@ -0,0 +1,12 @@
+package dinesh.varyani.graph;
+
+/**
+ * In this problem, we are given an m x n 2D binary grid,
+ * which represents a map of '1's (land) and '0's (water).
+ * We need to return the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ */
+//https://www.youtube.com/watch?v=iz0T9z3V84g&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=173&ab_channel=DineshVaryani
+public class NumberOfIslands {
+}
