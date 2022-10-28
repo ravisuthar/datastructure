35eddc72a9f63f3a971eef72b8d6d2dcbb8f31ca
sutha <!#bin/bash/git/88>
2022-10-28 11:47:32 +0530

    28 Oct 2022 Dynamic Programming (Kadan's Algorithm, fibonacci series, bottom up and top down approach).

---
+++ data-structure-algorithm/src/main/java/dinesh/varyani/dynamicprograming/KadanesAlgorithm.java
@@ -0,0 +1,41 @@
+package dinesh.varyani.dynamicprograming;
+
+public class KadanesAlgorithm {
+
+
+    //max value test
+    public static int subArray(int[] arr) {
+
+        int currentMax = arr[0];
+        int maxSoFar = arr[0];
+
+        int j=1;
+        while(j<arr.length){
+            currentMax=currentMax+arr[j];
+            if(currentMax >= arr[j]){
+                j++;
+            }else{
+                currentMax=arr[j];
+                j++;
+            }
+            if(currentMax > maxSoFar){
+                maxSoFar=currentMax;
+            }
+        }
+
+        //below using simple for loop
+        /*for (int i = 1; i < arr.length; i++) {
+            if(currentMax < arr[j]){
+                currentMax=arr[j];
+            }
+            if(currentMax > maxSoFar){
+                maxSoFar=currentMax;
+            }
+        }*/
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subArray(new int[]{13,3,-2,6,-12,7,-1,2136}));
+    }
+}
